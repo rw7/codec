9510d3c8c53315ac1fc74e1d38c0fafa42c1ba19
Wolfgang Buecke <wbuecke@gmail.com>
2014-09-11 20:37:27 +0200

    rename

--- src/com/thrivingcode/codec/Latin1Input.java
+++ src/com/thrivingcode/codec/Latin1Input.java
@@ -1,7 +1,7 @@
 package com.thrivingcode.codec;
 
 
-public class Latin1Input extends CharsetInput {
+public class Latin1Input extends CharsetHexInput {
 
 	public Latin1Input(Inputs inputs) {
 		super(inputs, "ISO-8859-1 (hex)", "ISO-8859-1");
--- src/com/thrivingcode/codec/Utf8Input.java
+++ src/com/thrivingcode/codec/Utf8Input.java
@@ -2,7 +2,7 @@ package com.thrivingcode.codec;
 
 import org.apache.commons.lang3.CharEncoding;
 
-public class Utf8Input extends CharsetInput {
+public class Utf8Input extends CharsetHexInput {
 
 	Utf8Input(Inputs inputs) {
 		super(inputs, "UTF-8 (hex)", CharEncoding.UTF_8);
